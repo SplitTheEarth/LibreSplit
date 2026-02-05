dfbb1869848a31f06d936a59ff661c21212d03b3
Aydan Miller <aydan.miller21@protonmail.com>
2026-02-04 20:39:40 -0500

    Update theming.h

--- src/gui/theming.h
+++ src/gui/theming.h
@@ -7,6 +7,5 @@ extern const unsigned char _binary____src_fallback_css_start[];
 extern const unsigned char _binary____src_fallback_css_end[];
 
 int ls_app_window_find_theme(const LSAppWindow* win, const char* name, const char* variant, char* out_path);
-void ls_app_refresh_theme(LSAppWindow* win);
 
 void ls_app_load_theme_with_fallback(LSAppWindow* win, const char* name, const char* variant);
